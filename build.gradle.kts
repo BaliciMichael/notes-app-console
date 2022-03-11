b7aa5c6db4b5b563b67163ecf72360a9cc7a8a26
BaliciMichael <balicimichael@gmail.com>
2022-03-11 13:49:42 +0000

    Logging capabilities added to the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,11 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    implementation("io.github.microutils:kotlin-logging:2.1.21")
+
+    implementation("org.slf4j:slf4j-simple:2.0.0-alpha6")
+
 }
 
 tasks.test {
